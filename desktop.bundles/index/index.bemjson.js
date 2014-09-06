f9f6e3dcad586462e7d3f84523244308e515f1e4
Pavel <php_prog@mail.ru>
2014-09-06 22:29:42 +0400

    init

---
+++ desktop.bundles/index/index.bemjson.js
@@ -0,0 +1,15 @@
+({
+    block: 'page',
+    title: 'Тренировка',
+    head: [
+        { elem: 'css', url: '_index.css' }
+    ],
+    scripts: [{ elem: 'js', url: '_index.js' }],
+    mods: { theme: 'normal' },
+    content: [
+
+        
+
+
+    ]
+})
